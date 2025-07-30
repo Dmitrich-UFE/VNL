1c168f59091ac2ce8d0be06699527005a22bac09
Евтухов Дмитрий <justarcane@vk.com>
2025-07-30 20:41:44 +0300

    Добавлена поддержка тега sps (вывод с определенным значением SymbolsPerSecond) и введено значение currentSymbolsPerSecond

--- Assets/VNL/Scripts/VNLPrinter.cs
+++ Assets/VNL/Scripts/VNLPrinter.cs
@@ -9,6 +9,7 @@ public class VNLPrinter : MonoBehaviour
 {
     public enum printStatus {Printing, Printed}
     private Queue<string> SymbolsQueue;
+    private int currentSymbolsPerSecond;
 
     //текстовое окно
     [SerializeField] private TMP_Text dialogueWindow;
@@ -29,6 +30,7 @@ public class VNLPrinter : MonoBehaviour
     void Awake()
     {
         SymbolsQueue = new Queue<string>();
+        currentSymbolsPerSecond = SymbolsPerSecond;
     }
     
     void Start()
@@ -81,19 +83,31 @@ public class VNLPrinter : MonoBehaviour
         switch (SymbolsQueue.Peek())
         {
             //ожидание клика
-            case string customTag when Regex.IsMatch(customTag, @"^ *< *wait *> *$"):
+            case string customTag when Regex.IsMatch(customTag, @"^ *< *wait *> *$", RegexOptions.IgnoreCase):
                 CancelInvoke("Printing"); 
                 SymbolsQueue.Dequeue();
                 _VNLClickHandler.OnClick += Continue;
                 break;
 
             //задержка вывода
-            case string customTag2 when Regex.IsMatch(customTag2, @"^ *< *delay *= *[^+-]\d+ *> *$"):
+            case string customTag2 when Regex.IsMatch(customTag2, @"^ *< *delay *= *[^+-]\d+ *> *$", RegexOptions.IgnoreCase):
                 CancelInvoke("Printing"); 
                 SymbolsQueue.Dequeue();
                 Delay(Convert.ToUInt32(Regex.Match(customTag2, @"(?<=\=) *\d+").Value));
                 break;
 
+            //печать с определенным количеством символов в секунду
+            case string customTag3 when Regex.IsMatch(customTag3, @"^ *< */? *sps *(= *[^+-]\d+ *)?> *$", RegexOptions.IgnoreCase):
+                CancelInvoke("Printing"); 
+                SymbolsQueue.Dequeue();
+
+                if (Regex.IsMatch(customTag3, @"^ *< *sps *(= *[^+-]\d+ *)?> *", RegexOptions.IgnoreCase))
+                    CurrentSPSPrint(Convert.ToUInt32(Regex.Match(customTag3, @"(?<=\=) *\d+").Value));
+
+                if (Regex.IsMatch(customTag3, @" *< */? *sps *> *", RegexOptions.IgnoreCase))
+                    CurrentSPSPrint(Convert.ToUInt32(SymbolsPerSecond));
+                break;
+                
             //другие теги:
             //оформляются подобным образом. Если нужен клик для срабатывания тега, то обязательно подписываемся на событие _VNLClickHandler.OnClick
             
@@ -113,11 +127,19 @@ public class VNLPrinter : MonoBehaviour
     void Continue()
     {
         _VNLClickHandler.OnClick -= Continue;
-        InvokeRepeating("Printing", 0f, 1f/SymbolsPerSecond);
+        InvokeRepeating("Printing", 0f, 1f/currentSymbolsPerSecond);
     }
 
+    //задержка текста в миллисекундах
     void Delay(uint milliseconds)
     {
-        InvokeRepeating("Printing", milliseconds/1000f, 1f/SymbolsPerSecond);
+        InvokeRepeating("Printing", milliseconds/1000f, 1f/currentSymbolsPerSecond);
+    }
+
+    //вывод текста со скоростью не по умолчанию
+    void CurrentSPSPrint(uint symbolsPerSecond)
+    {
+        currentSymbolsPerSecond = Convert.ToInt32(symbolsPerSecond);
+        InvokeRepeating("Printing", 0f, 1f/symbolsPerSecond);
     }
 }
